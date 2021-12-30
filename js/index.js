5f92597c8165cef26c53af38d7631dd2dad33656
hacenegaridi <hacenegaridi4710@gmail.com>
2021-12-30 16:05:57 +0100

    ajout des fichier Js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*let url = 'http://localhost:3000/api/products'; 
+
+//je demande à fetch de récupérer les données depuis l'url de l'API : 
+fetch('http://localhost:3000/api/products')
+.then(res => res.json())
+.then(data => { 
+    showProducts(data);
+  })
+    */
+   alert('Bonjour je suis le fichier js');
+   
